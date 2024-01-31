892f4e19c493855984d8da42080294a1caabafda
usung <usung418@naver.com>
2024-01-31 12:36:28 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+git remote add origin git@github.com:usung1/MyPortfolio.git
+git push origin masterg
+git remote add origin https://github.com/usung1/django-portfolio.git
+git remote add origin https://github.com/usung1/Portfolio.git
